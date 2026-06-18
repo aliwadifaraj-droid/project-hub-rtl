46b1691046fc0c44bab79bfee8978fddb940e155
aliwadifaraj-droid <aliwadifaraj@gmail.com>
2026-06-18 19:54:37 +0300

    Update submit-project.tsx

--- src/routes/submit-project.tsx
+++ src/routes/submit-project.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useServerFn } from "@tanstack/react-start";
 import { useState } from "react";
-import { submitProject } from "@/lib/projects.functions
+import { submitVisitorAd } from "@lib/ads.functions";
 import { supabase } from "@/integrations/supabase/client";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
