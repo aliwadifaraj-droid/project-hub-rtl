d2cf6abb0f3c2e0e0c8563d33d522ca6600106a4
aliwadifaraj-droid <aliwadifaraj@gmail.com>
2026-09-09 16:32:49 +0300

    Update vip.index.tsx

--- src/routes/vip.index.tsx
+++ src/routes/vip.index.tsx
@@ -16,7 +16,7 @@ import { SAUDI_CITIES } from "@/lib/saudi-cities";
 
 const BANK_INFO = {
   name: "البنك العربي",
-  holder: "",
+  holder: "AHMEDSALMI",
   iban: "SA8530100991109655808477",
 };
 
